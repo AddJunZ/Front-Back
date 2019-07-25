a6af7b56ef226b1dad12005a6704f6aea30dda45
AddJun <1574118221@qq.com>
2019-07-25 16:25:01 +0800

    初始框架已经正确了

--- koa-mongo/app.js
+++ koa-mongo/app.js
@@ -1,5 +1,11 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
 const app = new Koa();
+const router = new Router();
+
+router.use('/user',require('./routes/user'))
+app.use(router.routes())
 app.listen(3000,()=>{
     console.log('监听3000');
 })
